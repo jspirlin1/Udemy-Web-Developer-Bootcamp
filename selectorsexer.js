01dd62e6cb088dfb0738ce5a6605d43ad9151758
jspirlin1 <jay0701@gmail.com>
2018-04-11 15:00:15 -0700

    Add files via upload

---
+++ selectorsexer.js
@@ -0,0 +1,18 @@
+//Come up with 4 different ways to select 
+//the first <p> tag.
+
+// 1. getElementByID
+var p = document.getElementById("first");
+console.log(p);
+
+// 2. getElementsByClassName
+var p = document.getElementsByClassName("special")[0];
+console.log(p);
+
+// 3. getElementsByTagName
+var p = document.getElementsByTagName("p")[0];
+console.log(p);
+
+// 3. querySelector
+var p = document.querySelector("#first");
+console.log(p);
