f85df83b9dfbc6121e60b6aba58ad05c03a7d17a
jspirlin1 <jay0701@gmail.com>
2018-04-15 11:16:34 -0700

    Add files via upload

---
+++ Color Toggle/toggle.js
@@ -0,0 +1,11 @@
+var button = document.querySelector("button");
+var isPurple = false;
+
+button.addEventListener("click", function(){
+	//if white
+		//make it purple
+	//else
+		//make it whites	
+	document.body.classList.toggle("purple");
+		
+});
